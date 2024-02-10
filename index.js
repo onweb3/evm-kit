58f2097f09c1e9c6a73df462065a072927e4855b
onweb3 <onweb3s@gmail.com>
2024-02-11 02:27:57 +0400

    globalized

---
+++ index.js
@@ -0,0 +1,9 @@
+const importWallet = require("./src/importWallet");
+const createWallet = require("./src/walletCreate");
+const readContract = require("./src/contractRead");
+
+module.exports = {
+    importWallet,
+    createWallet,
+    readContract,
+}
